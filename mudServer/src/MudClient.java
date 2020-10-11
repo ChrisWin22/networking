25d1bef020de9aebc48cd5fd8ed717bfc7b3961a
Christian-Windows <christian.winwood@aggiemail.usu.edu>
2020-10-10 21:01:04 -0600

    switch to linux

--- mudServer/src/MudClient.java
+++ mudServer/src/MudClient.java
@@ -1,10 +1,34 @@
-
+import java.io.*;
+import java.net.Socket;
+import java.util.logging.SocketHandler;
 
 public class MudClient implements Runnable{
-
+    InputStream inputStream;
+    MudClient(InputStream in){
+        inputStream = in;
+    }
 
     @Override
     public void run() {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("achaea.com", 23);
+
+        InputStream in = socket.getInputStream();
+        OutputStream out = socket.getOutputStream();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out));
+
+        Thread thread = new Thread(new MudClient(in));
+        thread.start();
+
+        String line = "";
+        while((line = reader.readLine()) != null){
+            System.out.println(line);
+        }
 
     }
 }
