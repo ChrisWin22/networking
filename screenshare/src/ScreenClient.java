08573350321915383fb0cb3884bb0165a5ca23de
Christian Winwood <christianwinwood@gmail.com>
2020-11-17 23:11:36 -0700

    Server is working, client not displaying screen

--- screenshare/src/ScreenClient.java
+++ screenshare/src/ScreenClient.java
@@ -1,26 +1,87 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.*;
+import java.util.Arrays;
 
 public class ScreenClient {
+    static DatagramSocket socket;
+    static Frame frame;
+    static Canvas canvas;
+
+    public static class keepAlive extends Thread{
+        DatagramPacket heartBeat;
+
+        public keepAlive(){
+            byte[] words = "alive".getBytes();
+            heartBeat = new DatagramPacket(words, words.length);
+        }
+
+        @Override
+        public void run(){
+            try {
+                while(true) {
+                    socket.send(heartBeat);
+                    System.out.println("Sent heartbeat");
+                    Thread.sleep(2000);
+                }
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static BufferedImage getImage(byte[] bytes, int offset, int length) throws IOException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes, offset, length);
+        BufferedImage image = ImageIO.read(byteArrayInputStream);
+        byteArrayInputStream.close();
+        return image;
+    }
+
+//    public static BufferedImage scale(BufferedImage image){
+//        int canvasWidth = canvas.getWidth();
+//        int canvasHeight = canvas.getHeight();
+//
+//        int imageWidth = image.getWidth();
+//        int imageHeight = image.getHeight();
+//
+//
+//    }
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Starting Client...");
 //        InetAddress host = InetAddress.getByName(args[0);
 //        int port = Integer.parseInt(args[1]);
 
         InetAddress host = InetAddress.getByName("127.0.0.1");
-        int port = 27;
-        DatagramSocket socket = new DatagramSocket();
+        int port = 1024;
+        socket = new DatagramSocket();
 //        socket.setSoTimeout(10000);
         socket.connect(host, port);
-        DatagramPacket response = new DatagramPacket(new byte[1024], 1024);
+        new keepAlive().start();
         System.out.println("Client connected to: " + host.getHostAddress() + ":" + port);
 
+        frame = new Frame("Screenshare");
+        canvas = new Canvas();
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        canvas.setSize(dimension.width/2, dimension.height/2);
+        frame.add(canvas);
+        frame.pack();
+        frame.setVisible(true);
 
+
+        byte[] buf = new byte[1 << 16];
         while (true){
+            DatagramPacket response = new DatagramPacket(buf, buf.length);
             socket.receive(response);
-            System.out.println(new String(response.getData()));
+            BufferedImage receivedImage = getImage(response.getData(), response.getOffset(), response.getLength());
+//            BufferedImage scaledImage = scale(receivedImage);
+            canvas.paint(receivedImage.createGraphics());
+//            canvas.update(receivedImage.createGraphics());
+            Thread.sleep(90);
         }
 
     }
--- screenshare/src/ScreenServer.java
+++ screenshare/src/ScreenServer.java
@@ -1,9 +1,14 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
-import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
 
 public class ScreenServer {
     static ArrayList<Connection> activeConnections;
@@ -27,30 +32,51 @@ public class ScreenServer {
         }
     }
 
-    public static byte[] getBytesToSend(){
-        //TODO
-        BufferedImage image = robot.createScreenCapture(screenSize);
-        return new byte[1024];
-    }
-
     public static class Sender extends Thread{
+        static AffineTransformOp affineTransformOp;
+
+        public Sender(){
+            affineTransformOp = new AffineTransformOp(AffineTransform.getScaleInstance(.3,.3), AffineTransformOp.TYPE_BICUBIC);
+        }
+
+        public static BufferedImage scale(BufferedImage image){
+            int newWidth = (int) (image.getWidth() * .3);
+            int newHeight = (int) (image.getHeight() * .3);
+            BufferedImage newImage = new BufferedImage(newWidth, newHeight, image.getType());
+            newImage = affineTransformOp.filter(image, newImage);
+            return newImage;
+        }
+
+        public static byte[] getBytesToSend() throws IOException {
+            BufferedImage image = robot.createScreenCapture(screenSize);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            BufferedImage scaledImage = scale(image);
+            ImageIO.write(scaledImage, "gif", byteArrayOutputStream);
+            return byteArrayOutputStream.toByteArray();
+        }
+
         @Override
         public void run(){
-            byte[] bytesToSend = getBytesToSend();
-           synchronized (activeConnections){
-               for(Connection c : activeConnections){
-                   if(c.nextTimeout < System.currentTimeMillis()){
-                       activeConnections.remove(c);
-                       continue;
-                   }
-                   DatagramPacket response = new DatagramPacket(bytesToSend, bytesToSend.length, c.address, c.port);
-                   try {
-                       socket.send(response);
-                   } catch (IOException e) {
-                       e.printStackTrace();
-                   }
-               }
-           }
+            while(true) {
+                byte[] bytesToSend = new byte[0];
+                try {
+                    bytesToSend = getBytesToSend();
+                    for (Iterator<Connection> it = activeConnections.iterator(); it.hasNext();) {
+                        Connection c = it.next();
+                        if (c.nextTimeout < System.currentTimeMillis()) {
+                            it.remove();
+                            System.out.println("Removed " + c.address + ":" + c.port + " from active connections (timeout)");
+                            continue;
+                        }
+                        DatagramPacket response = new DatagramPacket(bytesToSend, bytesToSend.length, c.address, c.port);
+                        socket.send(response);
+                        System.out.println("Sent Message");
+                    }
+                    Thread.sleep(100);
+                } catch (IOException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
@@ -59,8 +85,8 @@ public class ScreenServer {
 
         System.out.println("Starting Server...");
         socket = new DatagramSocket(1024);
+        socket.setSendBufferSize(1024);
         robot = new Robot();
-        DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
         Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
         screenSize = new Rectangle(0,0, screenDimension.width, screenDimension.height);
         activeConnections = new ArrayList<>();
@@ -68,6 +94,7 @@ public class ScreenServer {
         System.out.println("Started on port 1024");
 
         while(true){
+            DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
             socket.receive(request);
             processHeartbeat(request.getAddress().getHostAddress(), request.getPort());
             System.out.println("Received heartbeat from " + request.getAddress().getHostAddress() + ":" + request.getPort());
@@ -76,17 +103,16 @@ public class ScreenServer {
 
     public static void processHeartbeat(String ip, int port) throws UnknownHostException {
         boolean found = false;
-        synchronized (activeConnections) {
-            for (Connection c : activeConnections) {
-                if (c.address.getHostAddress().compareTo(ip) == 0 && c.port == port) {
-                    c.receivedHeartbeat();
-                    found = true;
-                    break;
-                }
+        for (Connection c : activeConnections) {
+            if (c.address.getHostAddress().compareTo(ip) == 0 && c.port == port) {
+                c.receivedHeartbeat();
+                found = true;
+                break;
             }
-            if (!found) {
-                activeConnections.add(new Connection(ip, port));
-            }
+        }
+        if (!found) {
+            activeConnections.add(new Connection(ip, port));
+            System.out.println("Added " + ip + ":" + port + " to active connections");
         }
     }
 }
